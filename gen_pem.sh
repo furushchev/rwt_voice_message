fd755befaf90fc9afd92f473ede8ed1c2a96162d
Furushchev <furushchev@mail.ru>
2013-12-25 00:51:22 +0900

    revert to non-https

--- gen_pem.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-openssl req -new -x509 -subj="/C=AU/ST=Some-State/L=city/O=Dis/CN=www.example.com/" -keyout server.pem -out server.pem -days 365 -nodes
-mkdir config
-mv server.pem config
